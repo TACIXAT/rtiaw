a25b81ebf62e3e840e53fa37cde00b0cb4d27e7f
Douglas Goddard <douglas.gastonguay@gmail.com>
2016-01-31 16:21:10 -0500

    Add a sphere

--- main.cpp
+++ main.cpp
@@ -5,7 +5,18 @@
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image_write.h"
 
+bool hit_sphere(const vec3& center, float radius, const ray& r) {
+	vec3 oc = r.origin() - center;
+	float a = dot(r.direction(), r.direction());
+	float b = 2.0 * dot(oc, r.direction());
+	float c = dot(oc, oc) - radius*radius;
+	float discriminant = b*b - 4*a*c;
+	return (discriminant > 0);
+}
+
 vec3 color(const ray& r) {
+	if(hit_sphere(vec3(0.0, 0.0, -1.0), 0.5, r))
+		return vec3(0.0, 0.0, 1.0);
 	vec3 unit = r.direction().unit();
 	// normalize -1 to 1 -> 0 to 2 -> 0 to 1 
 	float t = 0.5*(unit.y() + 1.0);
