e7acd5b132d8d220275fdc99b425dccea4919b86
Douglas Goddard <douglas.gastonguay@gmail.com>
2016-01-31 16:38:41 -0500

    Color sphere

--- main.cpp
+++ main.cpp
@@ -5,21 +5,29 @@
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image_write.h"
 
-bool hit_sphere(const vec3& center, float radius, const ray& r) {
+float hit_sphere(const vec3& center, float radius, const ray& r) {
 	vec3 oc = r.origin() - center;
 	float a = dot(r.direction(), r.direction());
 	float b = 2.0 * dot(oc, r.direction());
 	float c = dot(oc, oc) - radius*radius;
 	float discriminant = b*b - 4*a*c;
-	return (discriminant > 0);
+	if(discriminant < 0) {
+		return -1.0;
+	} else {
+		return (-b - sqrt(discriminant)) / (2.0 * a);
+	}
 }
 
 vec3 color(const ray& r) {
-	if(hit_sphere(vec3(0.0, 0.0, -1.0), 0.5, r))
-		return vec3(0.0, 0.0, 1.0);
+	float t = hit_sphere(vec3(0.0, 0.0, -1.0), 0.5, r);
+	if(t > 0.0) {
+		vec3 v_color = (r.point_at_parameter(t) - vec3(0,0,-1)).unit();
+		return 0.5*(v_color+1.0);
+	}
+
 	vec3 unit = r.direction().unit();
 	// normalize -1 to 1 -> 0 to 2 -> 0 to 1 
-	float t = 0.5*(unit.y() + 1.0);
+	t = 0.5*(unit.y() + 1.0);
 	vec3 white = (1.0-t)*vec3(1.0, 1.0, 1.0);
 	vec3 blue = t*vec3(1.0, 0.2, 0.3);
 	return white+blue;
