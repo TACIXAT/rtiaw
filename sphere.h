aeb9a2d2e0d9f0a003e61d0dcdb5ca0edb0c9617
Douglas Goddard <douglas.gastonguay@gmail.com>
2016-02-09 22:08:13 -0500

    Delete commented out code.

--- sphere.h
+++ sphere.h
@@ -23,15 +23,7 @@ bool sphere::hit(const ray& r, float t_min, float t_max, hit_record& rec) const 
         if(tmp < t_max && tmp > t_min) {
             rec.t = tmp;
             rec.p = r.point_at_parameter(rec.t);
-            // if(rec.p.x() > (v_center.x() + radius) || rec.p.x() < (v_center.x() - radius))
-            //     return false;
-            // if(rec.p.y() > (v_center.y() + radius) || rec.p.y() < (v_center.y() - radius))
-            //     return false;
             rec.normal = ((rec.p - v_center) / radius).unit();
-            // std::cout << rec.p.x() << "\t" << rec.p.y() << "\t" << rec.p.z() << std::endl;
-            // std::cout << rec.normal.x() << "\t" << rec.normal.y() << "\t" << rec.normal.z() << std::endl << std::endl;
-            // std::cout << v_center.x() << "\t" << v_center.y() << "\t" << v_center.z() << std::endl;
-            // std::cout << radius << std::endl << std::endl;
             return true;
         }
         tmp = (-b + sqrt(discriminant))/a;
